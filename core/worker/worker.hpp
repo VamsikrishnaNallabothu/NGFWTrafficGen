d3e9f7400055e2bf472ef68addad0ecfabef468b
Vamsi krishna Nallabothu <VamsikrishnaNallabothu@users.noreply.github.com>
2025-12-10 15:42:53 -0800

    Update worker.hpp

--- core/worker/worker.hpp
+++ core/worker/worker.hpp
@@ -33,26 +33,27 @@ public:
         std::shared_ptr<FlowScheduler> flow_scheduler;
         std::shared_ptr<IMIXEngine> imix_engine;
         std::shared_ptr<MetricsCollector> metrics_collector;
+        int timestamp_dynfield_offset;
     };
-    
+
     Worker(const Config& config);
     ~Worker();
-    
+
     // Start worker thread
     bool start();
-    
+
     // Stop worker thread
     void stop();
-    
+
     // Wait for worker to finish
     void join();
-    
+
     // Check if worker is running
     bool is_running() const;
-    
+
     // Get worker statistics
     CoreStats get_stats() const;
-    
+
     // Get core ID
     uint32_t get_core_id() const;
 
@@ -61,28 +62,27 @@ private:
     std::atomic<bool> running_{false};
     std::atomic<bool> should_stop_{false};
     std::unique_ptr<std::thread> worker_thread_;
-    
+
     // Worker main loop
     void worker_loop();
-    
+
     // Transmit burst of packets
     uint16_t transmit_burst(rte_mbuf** mbufs, uint16_t count);
-    
+
     // Receive burst of packets (optional)
     uint16_t receive_burst(rte_mbuf** mbufs, uint16_t max_count);
-    
+
     // Process received packets
     void process_received_packets(rte_mbuf** mbufs, uint16_t count);
-    
+
     // Build and transmit packets for flows
     void process_flows();
-    
+
     // Build packet from template
     rte_mbuf* build_packet_from_template(const PacketTemplate& template_in);
-    
+
     // Set CPU affinity
     void set_cpu_affinity();
 };
 
 } // namespace trafficgen
-
